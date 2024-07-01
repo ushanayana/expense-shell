2cade26c2c194b4af66aafdd77a4c86f0510d208
ushanayana <ushanayana@gmail.com>
2024-07-01 22:32:10 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -36,3 +36,12 @@ VALIDATE $? "starting mysql"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
 VALIDATE $? "setting root password" 
+
+mysql -h db.daws78s.blog -uroot -pExpenseApp@1 -e 'showdatabases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then 
+mysql-secure-installation --set-root-pass ExpenseApp@1 
+VALIDATE $? "MySql Root Password setup"
+else 
+echo -e "Mysql Root password is already setup... $Y SKIPPING $N"
+fi
