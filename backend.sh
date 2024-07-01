b077f8b82add5649602cb709575756c7dbffd5b3
ushanayana <ushanayana@gmail.com>
2024-07-02 00:39:10 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -53,6 +53,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "downloading the code..."
 
 cd /app
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "extract the backend code"
 
